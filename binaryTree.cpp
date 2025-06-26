adf32c26c0ba277a7c99db13e13b11a881dcbef9
rismamutia <rismamutiadewi89@gmail.com>
2025-06-26 15:26:41 +0700

    Exit

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -87,8 +87,11 @@ public:
         //step 7: If the value in the data field of the new node is greater than that of the parent
         else if (x > parent->info)
         {
-            //7a: Make the right child of parent point ti the new node
-            parent->rightchild = newNode
+            //7a: Make the right child of parent point to the new node
+            parent->rightchild = newNode;
+
+            //7b: Exit
+            return;
         }
 
     }
