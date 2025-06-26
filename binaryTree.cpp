15ea33263cbbe82a6d22d39fc9a641cd2b91cb87
rismamutia <rismamutiadewi89@gmail.com>
2025-06-26 15:08:49 +0700

    membuat fungsi untuk mencari current node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -26,4 +26,20 @@ public:
     {
         ROOT = nullptr; // Initializing ROOT to null
     }
+
+    void search(int element, Node *&parent, Node *&currentNode)
+    {
+        //This function searches the currentNode of the specified Node  as well as the current Node of its parent
+        currentNode = ROOT;
+        parent = nullptr;
+        while ((currentNode != nullptr) && (currentNode->info != element))
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+                
+            
+        }
+    }
 }
