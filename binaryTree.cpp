485089adb5bf6d7a636778f7ab3d82c17a7e75b1
rismamutia <rismamutiadewi89@gmail.com>
2025-06-26 15:26:08 +0700

    Make the right child of parent point to the new node

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -88,7 +88,7 @@ public:
         else if (x > parent->info)
         {
             //7a: Make the right child of parent point ti the new node
-            
+            parent->rightchild = newNode
         }
 
     }
